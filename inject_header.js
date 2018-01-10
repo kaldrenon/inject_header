dc12294e38dfe5e361d35cf08d0286995fa3a34d
Andrew Fallows <andrew.fallows@intervarsity.org>
2018-01-10 12:43:14 -0500

    [Urbana] modify with BoldChat code

--- inject_header.js
+++ inject_header.js
@@ -1,1 +1,34 @@
-console.log('loaded the JS HOOORAY !!!');
+// var bccbId = Math.random(); document.write(unescape('%3Cdiv class="bold-chat" id=' + bccbId + '%3E%3C/div%3E'));
+// window._bcvma = window._bcvma || [];
+// _bcvma.push(["setAccountID", "442519844613475880"]);
+// _bcvma.push(["setParameter", "WebsiteID", "439887189785208352"]);
+// _bcvma.push(["addStatic", {type: "chat", bdid: "439887189045499265", id: bccbId}]);
+// var bcLoad = function(){
+//   if(window.bcLoaded) return; window.bcLoaded = true;
+//   var vms = document.createElement("script"); vms.type = "text/javascript"; vms.async = true;
+//   vms.src = ('https:'==document.location.protocol?'https://':'http://') + "vmss.boldchat.com/aid/442519844613475880/bc.vms4/vms.js";
+//     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(vms, s);
+// };
+// if(window.pageViewer && pageViewer.load) pageViewer.load();
+// else if(document.readyState=="complete") bcLoad();
+// else if(window.addEventListener) window.addEventListener('load', bcLoad, false);
+// else window.attachEvent('onload', bcLoad);
+
+<!-- BoldChat Visitor Monitor HTML v5.00 (Website=urbana.org,Ruleset=My Invite Ruleset,Floating Chat=Andrew's Test Floating Chat Button -->
+window._bcvma = window._bcvma || [];
+_bcvma.push(["setAccountID", "442519844613475880"]);
+_bcvma.push(["setParameter", "WebsiteID", "439887189785208352"]);
+_bcvma.push(["setParameter", "InvitationID", "439887189794967361"]);
+_bcvma.push(["addFloat", {type: "chat", id: "439921957259324044"}]);
+_bcvma.push(["pageViewed", document.location.href, document.referrer]);
+var bcLoad = function(){
+  if(window.bcLoaded) return; window.bcLoaded = true;
+  var vms = document.createElement("script"); vms.type = "text/javascript"; vms.async = true;
+  vms.src = ('https:'==document.location.protocol?'https://':'http://') + "vmss.boldchat.com/aid/442519844613475880/bc.vms4/vms.js";
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(vms, s);
+};
+if(window.pageViewer && pageViewer.load) pageViewer.load();
+else if(document.readyState=="complete") bcLoad();
+else if(window.addEventListener) window.addEventListener('load', bcLoad, false);
+else window.attachEvent('onload', bcLoad);
+<!-- /BoldChat Visitor Monitor HTML v5.00 -->
